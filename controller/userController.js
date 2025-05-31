5892426b3d7cbc6fc8117f53c8fb93a153c727de
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-05-31 14:04:03 +0900

    feat: add auth user routes

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,22 @@
-export const authUser = (req, res) => {
+import axios from "axios";
+export const authUser = async (req, res) => {
   const { idNum } = req.body;
 
-  res.status(200).json({ message: "ID Number found", data: idNum });
+  try {
+    const response = await axios.post(
+      "https://auth.app.dlsu-lscs.org/member-id",
+      { id: 12308293 },
+      {
+        headers: {
+          Authorization: `Bearer <API-KEY>`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log(response);
+    res.status(200).json({ message: "ID Number found", data: response });
+  } catch {
+    res.status(200).json({ message: "ID number not found" });
+  }
 };
